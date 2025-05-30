c7984aea0ce52aa6ccf8583d922497bd9d9ec2e5
Cleber Gomes <cleber_slip@hotmail.com>
2025-05-30 08:11:02 -0300

    JS interativo

--- script.js
+++ script.js
@@ -1,3 +1,11 @@
-document.getElementById('btn').onclick = function() {
-  alert('Pré-Cadastro realizado! Compareça ao PEC para finalizar o cadastro e entregar os documentos.');
+//MENSAGEM AO CLICAR NO BOTÃO "ENVIAR"
+
+const aluno = document.getElementById('nome');
+const btn = document.getElementById('btn');
+
+btn.onclick = function() {
+  const inputAluno = aluno.value;
+  alert(`Bem vindo, ${inputAluno}! Pré-Cadastro realizado! Compareça ao PEC para finalizar o cadastro e entregar os documentos.`);
 };
+
+
